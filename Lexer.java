b49ab18ca775579959d148c19cfd01a41276a9c7
Michael Shapiro <michaeladamshapiro@gmail.com>
2022-02-02 20:39:06 -0500

    New Commit

--- Lexer.java
+++ Lexer.java
@@ -2,6 +2,7 @@ package edu.ufl.cise.plc;
 
 import edu.ufl.cise.plc.Token;
 
+import java.util.HashMap;
 import java.util.Map;
 import edu.ufl.cise.plc.IToken.Kind;
 
@@ -24,10 +25,46 @@ public class Lexer implements ILexer
     }
 
     private State state;
-    Map<String, Kind> reservered_map;
+    Map<String, Kind> reservered_map = new HashMap<String, Kind>()
+    {
+        {
+            put("string", Kind.TYPE);
+            put("int", Kind.TYPE);
+            put("float", Kind.TYPE);
+            put("boolean", Kind.TYPE);
+            put("color", Kind.TYPE);
+            put("image", Kind.TYPE);
+            put("void", Kind.TYPE);
+            put("getWidth", Kind.IMAGE_OP);
+            put("getHeight", Kind.IMAGE_OP);
+            put("getRed", Kind.COLOR_OP);
+            put("getBlue", Kind.COLOR_OP);
+            put("getGreen", Kind.COLOR_OP);
+            put("BLACK", Kind.COLOR_CONST);
+            put("BLUE", Kind.COLOR_CONST);
+            put("CYAN", Kind.COLOR_CONST);
+            put("DARK_GRAY", Kind.COLOR_CONST);
+            put("GRAY", Kind.COLOR_CONST);
+            put("GREEN", Kind.COLOR_CONST);
+            put("LIGHT_GRAY", Kind.COLOR_CONST);
+            put("MAGENTA", Kind.COLOR_CONST);
+            put("ORANGE", Kind.COLOR_CONST);
+            put("PINK", Kind.COLOR_CONST);
+            put("RED", Kind.COLOR_CONST);
+            put("WHITE", Kind.COLOR_CONST);
+            put("YELLOW", Kind.COLOR_CONST);
+            put("true", Kind.BOOLEAN_LIT);
+            put("false", Kind.BOOLEAN_LIT);
+            put("if", Kind.KW_IF);
+            put("else", Kind.KW_ELSE);
+            put("fi", Kind.KW_FI);
+            put("write", Kind.KW_WRITE);
+            put("console", Kind.KW_CONSOLE);
+        }
+    };
     Lexer(String source)
     {
-        
+
     }
 
     @Override
--- LexerTests.java
+++ LexerTests.java
@@ -1,4 +1,4 @@
-package edu.ufl.cise.plc.test;
+package edu.ufl.cise.plc;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
